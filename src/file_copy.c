33a35c805e89603de0d2efd3e6ca517b685f8669
Meluleki <mellarmellah@gmail.com>
2019-01-15 18:17:22 +0200

    Started off with the initial Excercises.

---
+++ src/file_copy.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void ex17(){
+	printf("%i\n", EOF);
+}
+
+void count_spechars(char *filename){
+	int tabs, spaces, nl, ch;
+	tabs = spaces = nl = 0;
+	++nl;
+	FILE *file = fopen(filename, "r");
+	while((ch = fgetc(file))!= EOF){
+		switch(ch){
+			case ' ':
+				++spaces;
+				break;
+			case '\t':
+				++tabs;
+				break;
+			case '\n':
+				++nl;
+				break;
+				
+		}
+	}
+	fclose(file);
+	printf("Spaces is: %d, Tabs is: %d, New lines is: %d, Total is: %d\n", spaces, tabs, nl, (spaces+tabs+nl));
+}
+
+
+int char_count(char *filename){
+	FILE *f =fopen(filename, "r");
+	int c, char_number;
+	char_number = 0;
+	while((c=fgetc(f)) != EOF){
+		++char_number;
+	}
+	fclose(f);
+	return char_number;
+}
+
+double char_count2(char* filename){
+	FILE *f = fopen(filename, "r");
+	double nc;
+	for(nc =0; fgetc(f)!= EOF; ++nc);
+	fclose(f);
+	return nc;
+}
+
+int line_count(char* filename){
+	FILE *file = fopen(filename,"r");
+	int ln, ch;
+	while((ch = fgetc(file))!=EOF){
+		if(ch == '\n')
+			++ln;
+	}
+	fclose(file);
+	return ln;
+}
+
+void copy_and_replace(char *srcfname, char* desname){
+	/*ex1-9*/
+	FILE *srcfile = fopen(srcfname, "r");
+	FILE *destfile = fopen(desname, "w");
+	int c;
+	while((c=fgetc(srcfile)) != EOF){
+		fputc(c, destfile);
+		if(c ==' '){
+			while((c=fgetc(srcfile))==' ');
+			if(c == EOF)
+				break;
+			fputc(c, destfile);
+		}
+	}
+	fclose(srcfile);
+	fclose(destfile);
+}
+
+void copy_and_replace2(char *srcfname, char* desname){
+	/* ex1-10 */
+	FILE *srcfile = fopen(srcfname, "r");
+	FILE *destfile = fopen(desname, "w");
+	int c;
+	while((c=fgetc(srcfile)) != EOF){
+		
+		switch(c){
+			case '\t':
+				fputc('\\', destfile);
+				fputc('t', destfile);
+				break;
+			case '\b':
+				fputc('\\', destfile);
+				fputc('b', destfile);
+				break;
+			case '\\':
+				fputc('\\', destfile);
+				fputc('\\', destfile);
+				break;
+			default:
+				fputc(c, destfile);
+		}
+	}
+	fclose(srcfile);
+	fclose(destfile);
+}
+
+void copy(char *srcfname, char* desname){
+	FILE *src = fopen(srcfname, "r");
+	FILE *dest = fopen(desname, "w");
+	int c;
+	while((c = fgetc(src))!= EOF){
+		fputc(c, dest);
+	}
+	fclose(src);
+	fclose(dest);
+}
+
+int main(int argc, char*argv[]){
+	ex17();
+	copy_and_replace(argv[1], argv[2]);
+	count_spechars(argv[1]);
+	return 0;
+}
---
+++ src/temp_unitconv.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+float convert_temp(float F);
+
+int main1(int argv, char*argc[]){
+	float celc, fehr;
+	int lower, upper, step;
+	printf("%i\n", argv);
+	if(argv == 2){
+		fehr = atoi(argc[1]);
+		celc = convert_temp(fehr);
+		printf("Converting input %f F to %f C\n", fehr, celc);
+	}else{
+		lower =0; /* lower limit of the temperature table*/
+		upper = 300; /* upper limit of the temperature table*/
+		step = 20; /* increment of the temperature table*/
+		printf("************** Temperature conversion Table ");
+		printf("**************\n");
+		while(lower <= upper){
+			fehr = lower;
+			celc = convert_temp(fehr);
+			printf("%3.0f %6.1f\n", fehr, celc);
+			lower += step;
+		}
+	}
+}
+
+float convert_temp(float f){
+	return (5.0/9.0)*(f-32);
+}
