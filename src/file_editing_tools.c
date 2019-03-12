0fdef130745d26d4425447ac092aeab1ed72c7f0
Meluleki <mellarmellah@gmail.com>
2019-03-12 11:38:04 +0200

    Added the solution to folding of lines.the solutions doesn't work if it finds a line without space greater than the limit.

--- src/file_editing_tools.c
+++ src/file_editing_tools.c
@@ -3,22 +3,25 @@
 #include <stdbool.h>                                              
 #define line_length  64
 #define TABSTOPS 4
-#define MAXLINE 2000
+#define MAXLINE 65536 //random number
 
 typedef char* string;
 void detab(char s[], int limit);/*ex1.20*/
 void entab(char s[], int limit);  /*ex1.21*/
 int _get_line(char s[], int limit); /*ex 1.22*/
-void fold(char s[], int n);
+void fold(char file_name[], char save_file [], int limit);
 int get_file_line(FILE* file, char buffer[], int maxline);
 
 void remove_comments(char file_name[], char save_file []); /*ex1.23*/
+
+
+static bool done = false;
 
 int main(int argc, char *args[]){
 	printf("Running file_editing_tools\n");
 	char *f1 = args[1];
 	char *f2 = args[2];
-    remove_comments(f1, f2);
+    fold(f1, f2, 10);
 }
 
 int _get_line(char s[], int limit){
@@ -61,9 +64,8 @@ void entab(char s[], int limit){
     int counter=1, index=0, ns=0, c;
     for(;counter<limit-1&&(c=getchar())!=EOF&&c!='\n';++counter){
         if(c==' '){
-            ++ns;         
-//            printf("ns value is: %i, counter value is: %i\n", ns, counter);
-            if(!(counter%TABSTOPS)){//we will deal ==1 later
+            ++ns; 
+            if(!(counter%TABSTOPS)){
                 if(ns>1){
                     s[index]='\t';
                 }else if(ns==1){
@@ -89,15 +91,67 @@ void entab(char s[], int limit){
     s[index] = '\0';
 }
 
-void fold(char s[], int n){
+int process_fold(FILE* file, char buffer[], int buff_limit, int line_limit) {
+	int i, j, counter, c;//reason for c being int is that EOF is an int and not a char.
 	
+	counter  = 0;
+	for(i=0;(c=fgetc(file))!=EOF&&c!='\n';++i){
+		buffer[i] = c;
+		if(c=='\t')
+			counter+=TABSTOPS;
+		else ++counter;
+		if(counter>=line_limit-1){
+			if(c==' '||c=='\t'){
+				buffer[i] = '\n';
+				++i;
+				break;
+			}else{
+				//we wanna trace back to the last empty space and place the null terminator there
+				j = i;
+				while(j>=0&&buffer[j]!=' '&&buffer[j]!='\t'){
+					--j;
+				}
+				if(j>=0){
+					buffer[j] = '\n';
+					counter = 0;
+				}
+			}
+		}
+	}
+	
+	if(c=='\n'){
+		buffer[i] = '\n';
+		++i;
+	}
+	if(c==EOF)
+		done = true;
+	buffer[i] = '\0';
+	return i;
 }
-bool done = false;
+
+//Exercise 1-22.
+void fold(char file_name[], char save_file [], int line_limit){
+	int i, j;
+	FILE* input_file;
+	FILE* output_file;
+	
+	char buffer[MAXLINE];
+	input_file = fopen(file_name, "r");
+	output_file = fopen(save_file, "w");
+	process_fold(input_file, buffer, MAXLINE, line_limit);
+	while(!done){
+		fprintf(output_file, "%s", buffer);
+		process_fold(input_file, buffer, MAXLINE, line_limit);
+	}
+}
+
 int get_file_line(FILE* file, char buffer[], int maxline){
-	int c, i, j = 0;
-	static bool multi_line_comment = false; //will keep it's previous value on each function call
+	int c, i, j;
+	static bool in_string;
 	char comments[maxline];
-	for(i=0; (c=fgetc(file))!=EOF&&c!='\n';){
+	
+	static bool multi_line_comment = false; //will keep it's previous value on each function call
+	for(i=j=0; (c=fgetc(file))!=EOF&&c!='\n';){
 		//catch //
 		if(c=='/'&&((i-1>=0)&&buffer[i-1]=='/')){
 			//replace buffer[i-1] with newline char, terminate the string and return as the comment continues for rest of line
@@ -106,12 +160,15 @@ int get_file_line(FILE* file, char buffer[], int maxline){
 			buffer[i] ='\0';
 			while((c=fgetc(file))!=EOF&&c!='\n');
 			return i;
-		}else if(c=='*'&&((i-1>=0)&&buffer[i-1]=='/')){ //catching/**/
+		}else if(!in_string&&c=='*'&&((i-1>=0)&&buffer[i-1]=='/')){ //catching/**/
 			multi_line_comment = true;
 			--i;
 		}else if(multi_line_comment && c== '/' && (j>0) && comments[j-1]=='*'){
 			multi_line_comment = false;
 		}else if(!multi_line_comment){
+			if(c=='\"'){
+				in_string = in_string?false:true;
+			}
 			buffer[i] = c;
 			++i;
 		}else{
@@ -119,7 +176,7 @@ int get_file_line(FILE* file, char buffer[], int maxline){
 			++j;
 		}
 	}
-	if(c=='n'){
+	if(c=='\n'){
 		printf("yes");
 		buffer[i]='\n';
 		++i;
@@ -130,6 +187,7 @@ int get_file_line(FILE* file, char buffer[], int maxline){
 	return i;
 }
 
+//Exercise 1-23.
 void remove_comments(char file_name[], char save_file []){
     FILE *file = fopen(file_name,"r");
 	FILE *file_save = fopen(save_file, "w");
